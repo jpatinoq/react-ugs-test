0ea5260d3c6acf6a7ba1bf0b629c4501b3f2253e
Jorge Patino <jorgepatino@192.168.1.9>
2022-11-23 22:59:59 -0500

    Updates

---
+++ precache-manifest.6b4ac1493de018f3c6db067aa36e1bc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392772f8ce0c1d8006eaa8636e972eb2",
+    "url": "/react-ugs-test/index.html"
+  },
+  {
+    "revision": "5017b9b4e75e1506ac57",
+    "url": "/react-ugs-test/static/css/2.fcafdf31.chunk.css"
+  },
+  {
+    "revision": "b788e5fa578e6aadfac9",
+    "url": "/react-ugs-test/static/css/main.1a321600.chunk.css"
+  },
+  {
+    "revision": "5017b9b4e75e1506ac57",
+    "url": "/react-ugs-test/static/js/2.4a347082.chunk.js"
+  },
+  {
+    "revision": "112b9e655310c562d28b7db550db74b9",
+    "url": "/react-ugs-test/static/js/2.4a347082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b788e5fa578e6aadfac9",
+    "url": "/react-ugs-test/static/js/main.69103d79.chunk.js"
+  },
+  {
+    "revision": "cd564e5722352efebaae",
+    "url": "/react-ugs-test/static/js/runtime-main.0d47a4f1.js"
+  }
+]);
